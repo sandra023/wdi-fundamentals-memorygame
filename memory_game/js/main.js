3388d1f2761d32c9ce0bb55058e18caa608e3da6
Sandra Abernathy <Sandra@Sandras-iMac.local>
2019-06-06 12:34:05 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,37 @@
-console.log("Up and running!")
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
 
+
+var cards = ["Queen","Queen","King","King"];
+//cards on the board
+
+var cardsInPlay = [];
+//holds the cards in play
+
+var cardOne = cards[0];
+//first card the user picks
+
+cardsInPlay.push("cardOne");
+//sends the users first pick to cardsInPlay array
 
 console.log("User Flipped " + cardOne);
+//tells the user what they picked
+
+var cardTwo = cards[2];
+//second card the user pics
+
+cardsInPlay.push("cardTwo");
+//sends the users second pick to cardsInPlay array
+
+console.log("User Flipped " + cardTwo);
+//tells the user what they picked
+
+if (cardsInPlay.length === 2 && cardOne === cardTwo){
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+
+
+console.log(cardsInPlay);
+
 console.log("User Flipped " + cardThree);
